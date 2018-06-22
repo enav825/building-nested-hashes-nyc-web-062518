8d3820981023b03f9c2e160252b791d052fd6898
Ena Anicama <ena.v825@gmail.com>
2018-06-22 03:50:56 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,6 +24,17 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        steven = {
+          name: 'Steven',
+          age: '30',
+          attitude: 'confused'
+        },
+        nurse = {
+          name: 'Nurse',
+          age: '44',
+          attitude: 'worried'
+          
+        }
       ]
    }
 }
