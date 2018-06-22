f881c7a09aa5682fe10585180b706438e58e55ff
Ena Anicama <ena.v825@gmail.com>
2018-06-22 05:15:12 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,9 +21,9 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy [:montague] [:patriarch] [:matriarch] [:hero] [:status] = "dead"
+epic_tragedy [:montague] [:hero] [:status] = "dead"
 
-epic_tragedy [:capulet] [:patriarch] [:matriarch] [:heroine] [:status] = "dead"
+epic_tragedy [:capulet] [:heroine] [:status] = "dead"
   
 
   
